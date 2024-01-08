33b71cd4e3a01b34dd3b9cf317f83636af766144
Varya Utkina <varya2412@mail.ru>
2024-01-08 15:35:00 +0300

    Correct DevelopersViewController

--- SwiftProject/ViewControllers/DevelopersViewController.swift
+++ SwiftProject/ViewControllers/DevelopersViewController.swift
@@ -24,14 +24,16 @@ final class DevelopersViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let typesOfSections = Set(team.map {$0.role})
-        let sortedSections = Array(typesOfSections)
+        let sortedSections = typesOfSections.sorted { $0.rawValue > $1.rawValue }
         let numberOfRows: Int
+        
         switch sortedSections[section] {
         case .teamLead:
             numberOfRows = 1
         default:
             numberOfRows = 5
         }
+        
         return numberOfRows
     }
     
@@ -47,29 +49,21 @@ final class DevelopersViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "devCell", for: indexPath)
-        
+        var person: Developer
         
         if team[indexPath.section].role == .teamLead {
             let teamLead = team.filter { $0.role == .teamLead }
-            let person = teamLead[indexPath.section]
-            
-            var content = cell.defaultContentConfiguration()
-            content.text = person.name
-            content.secondaryText = person.developedQuiz
-            content.image = UIImage(named: person.photo)
-            content.imageProperties.cornerRadius = tableView.rowHeight / 2
-            cell.contentConfiguration = content
+            person = teamLead[indexPath.section]
         } else {
             let developers = team.filter { $0.role == .developer }
-            let person = developers[indexPath.row]
-            
-            var content = cell.defaultContentConfiguration()
-            content.text = person.name
-            content.secondaryText = person.developedQuiz
-            content.image = UIImage(named: person.photo)
-            content.imageProperties.cornerRadius = tableView.rowHeight / 2
-            cell.contentConfiguration = content
+            person = developers[indexPath.row]
         }
+        var content = cell.defaultContentConfiguration()
+        content.text = person.name
+        content.secondaryText = person.developedQuiz
+        content.image = UIImage(named: person.photo)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        cell.contentConfiguration = content
         
         return cell
     }
